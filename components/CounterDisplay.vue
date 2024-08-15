99a0df8595b330bc260e509be51e17ecc24a9bef
Ben Greenow <ben.greenow@hazcheck.com>
2024-08-15 17:36:25 +0100

    white text

--- components/CounterDisplay.vue
+++ components/CounterDisplay.vue
@@ -2,7 +2,10 @@
   <div class="flex flex-col items-center justify-center">
     <img :src="image" v-if="image" alt="Counter Logo" />
     <h2 v-if="title">{{ title }}</h2>
-    <SingleCounter class="text-6xl font-bold" :model-value="number" />
+    <SingleCounter
+      class="text-6xl font-bold text-white"
+      :model-value="number"
+    />
   </div>
 </template>
 
